4d65b232c4a1fa449ab08e81efdcd37088b8a770
Idrissou Dalia <47243715+d-al-i-a@users.noreply.github.com>
2020-03-06 12:20:20 +0100

    last version

--- tests/Unit/ExampleTest.php
+++ tests/Unit/ExampleTest.php
@@ -14,6 +14,58 @@ class ExampleTest extends TestCase
      */
     public function testBasicTest()
     {
+<<<<<<< Updated upstream
         $this->assertTrue(true);
     }
+=======
+
+        $use = new User([
+            'name' => 'admin',
+            'email' => 'admin@esi.dz',
+            'password' => 'password'
+            ]);
+
+            $this->actingAs($use);
+            $respons = $this->json('POST', '/ajouter', [
+            'nom'=>'Amirat',
+            'prenom'=>'rima',
+            'email'=>'amirat@esi.dz',
+            'adress'=>'tiziouzou',
+            'password'=>'rima',
+            'matricule'=>'170011',
+            'groupe'=> '6',
+            'promo'=>'1CS'
+            ]);
+            $this->assertDatabaseHas('users',['email'=>"amirat@esi.dz"]);
+            $respons->assertStatus(302);
+           
+    }
+
+
+    public function AfficherNotesTest()
+    {
+        $etud = new Etudiant([
+            'nom'=>'idrissou',
+            'prenom'=>'dalia',
+            'email'=>'hd_idrissou@esi.dz',
+            'adress'=>'bejaia',
+            'password'=>'dalia',
+            'matricule'=>'171717',
+            'groupe'=> '6',
+            'promo'=>'1CS'
+            ]);
+
+            $this->actingAs($etud); 
+            $response = $this->call('GET', '/get_marks');    
+            $response 
+            ->assertStatus(200)
+            ->assertSeeTexte('idrissou')
+            ->assertSeeTexte('Moyenne');
+
+    }                   
+
+    
+
+
+>>>>>>> Stashed changes
 }
